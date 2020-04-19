53bb9ba5c230666d709615f869463ba660f49b53
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-04-19 14:50:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require "pry"
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :amount, :status
+
+def initialize(sender, receiver, amount)
+@sender = sender
+@receiver = receiver
+@amount = amount
+@status = "pending"
 end
+
+def valid?
+ @sender.valid? && @receiver.valid?
+ 
+end
+
+def execute_transaction
+ if !(@status == "complete") && self.valid? && sender.balance >= amount
+  @receiver.deposit(amount)
+  @sender.balance -= amount
+  @status = "complete"
+else
+  @status = "rejected"
+  "Transaction rejected. Please check your account balance."
+ end 
+ 
+end
+
+def closed_account_trasfer 
+  
+end 
+
+
+
+#binding pry  
+
+end
+
