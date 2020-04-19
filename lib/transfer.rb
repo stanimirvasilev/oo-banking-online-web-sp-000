271166941c7db390f6d493a06346c09402365e96
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-04-19 14:51:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ def execute_transaction
   @sender.balance -= amount
   @status = "complete"
 else
-  @status = "rejected"
+  
   "Transaction rejected. Please check your account balance."
  end 
  
